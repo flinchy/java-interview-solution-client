1f289678d0fc4764db5092cf11c9f20897699448
chisom <iwowochisom@gmail.com>
2020-01-19 21:25:35 +0100

    feature(style-card-details): style card details

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="container">
       <div className="content_wrapper">
-          This is the content wrapper area
-          <div className="card_input">
-            <input className="input"/>
+        <div className="card_input">
+          <input className="input" />
+          <span className="desc">
+            Enter the first digits of a card number(BIN/IIN)
+          </span>
+        </div>
+        <div className="card_details__container">
+          <div className="scheme">
+            <p className="light_dark title">SCHEME/NETWORK</p>
+            <span className="card_value">Visa</span>
           </div>
+
+          <div className="type">
+            <p className="light_dark title">TYPE</p>
+            <span className="card_value">Debit/Credit</span>
+          </div>
+
+          <div className="bank">
+            <p className="light_dark title">BANK</p>
+            <span className="card_value">Jyske Bank, Hjörring</span>
+          </div>
+        </div>
+
+        <div className="card_details__container_two">
+          <div className="scheme">
+            <p className="light_dark title">BRAND</p>
+            <span className="card_value">Visa/Dankort</span>
+          </div>
+
+          <div className="type">
+            <p className="light_dark title" >PREPAID</p>
+            <span className="card_value">YES/NO</span>
+          </div>
+
+        </div>
       </div>
     </div>
   );
