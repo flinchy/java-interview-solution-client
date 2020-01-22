699c8b1dd335ad47e406d971fc9141dc19f5cb9a
chisom <iwowochisom@gmail.com>
2020-01-22 10:42:28 +0100

    wrapp all component with context

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React, {Fragment} from "react";
-import Card from './Card';
+import React, { Fragment } from "react";
+import Card from "./Card";
+import CardState from "./context/card/CardState";
+import AlertState from "./context/alert/AlertState";
+import Alert from "./pages/Alert";
+import NumberOfHits from "./NumberOfHits";
 
 function App() {
- 
   return (
-      <Fragment>
-        <Card />
-      </Fragment>
+    <Fragment>
+      <CardState>
+        <AlertState>
+          <Alert />
+          <div style={{
+            width: '85%',
+            margin: '4rem auto',
+            padding: '2rem 0',
+            boxShadow: '0px 3px 6px #00000029',
+            background: '#fff'
+          }}>
+            <Card />
+            <NumberOfHits />
+          </div>
+        </AlertState>
+      </CardState>
+    </Fragment>
   );
 }
 
