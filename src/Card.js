add46e9ceca06efade9c07b0bb4002e7d8c0668e
chisom <iwowochisom@gmail.com>
2020-02-03 18:19:45 +0100

    modifification

--- src/Card.js
+++ src/Card.js
@@ -44,6 +44,7 @@ const Card = () => {
   return (
     <div className="container">
       <div className="header">
+        <p>A card verification application</p>
         <h2 className="title_header">Verify Card (BINN/IIN)</h2>
 
         {cardData.success ? (
